56c54457547a015485b0bf681c99dade340c38a0
icednut <wglee21g@gmail.com>
2022-12-22 23:19:51 +0900

    initial commit

---
+++ src/test/scala/io/icednut/TyrianexerciseTests.scala
@@ -0,0 +1,9 @@
+package io.icednut
+
+class TyrianexerciseTests extends munit.FunSuite {
+
+  test("dummy test") {
+    assert(1 == 1)
+  }
+
+}
---
+++ tyrianapp.js
@@ -0,0 +1,5 @@
+import {
+  TyrianApp
+} from './target/scala-3.2.0/tyrianexercise-fastopt.js';
+
+TyrianApp.launch("myapp");
